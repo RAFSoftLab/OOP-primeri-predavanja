021e3da861ee66c68460f5e007190ba7bdfedcf3
Bojana DS <bdimicsurla@raf.rs>
2021-03-20 11:14:43 +0100

    nedelja3-demo

--- OOP-nedelja3/src/objectdemo/SimpleObjectDemo.java
+++ OOP-nedelja3/src/objectdemo/SimpleObjectDemo.java
@@ -12,14 +12,14 @@ public class SimpleObjectDemo {
 		Integer br = Integer.valueOf(4);
 		System.out.println(br instanceof Integer);  // true
 		System.out.println(br instanceof Object);  // true
-		System.out.println("abc" instanceof String); // string literal je instanca kalse String
-		System.out.println("abc" instanceof Object); // ali i klase Object
+		System.out.println("abc" instanceof String); // true string literal je instanca klase String
+		System.out.println("abc" instanceof Object); // true, ali i klase Object
 		Zaposleni z = new Zaposleni("Marko", "Mikic");
-		System.out.println(z instanceof Zaposleni);
-		System.out.println(z instanceof Object);		
+		System.out.println(z instanceof Zaposleni); // true
+		System.out.println(z instanceof Object);	// true
 		Object obj = new Zaposleni("Ana","Maric");
-		System.out.println(obj instanceof Zaposleni);
-		System.out.println(obj instanceof Object);
+		System.out.println(obj instanceof Zaposleni);  // true		
+		System.out.println(obj instanceof Object);    // true
 		
 	}
 
